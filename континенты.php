ac14509c9aa4daa21b0f2edb37389550300165c9
Olga Kosheleva <aproxim@rambler.ru>
2018-02-22 02:24:20 +0400

    first commit

--- континенты.php
+++ континенты.php
@@ -1,10 +1,10 @@
-<h1>Жестокое обращение с животными</h1>
+<h1>Animal abuse</h1>
 <?php
 
 $continents = array("Africa" =>  array("Okapia", "Dorylus helvolus", "Ixodes dawesi"),
 "Antarctica" => array("Sennertia antarctica", "Aptenodytes forsteri", "Tullbergia mediantarctica"),
-"Australia" => array(),
-"South_America" =>array(),
+"Australia" => array("Prototroctes oxyrhynchus", "Huberia", "Deinacrida"),
+"South_America" =>array("Chrysophora chrysochlora", "Saguinus imperator", "Hydrochoerus"),
 "North_America" => array("Pachybrachis calcaratus", "Gnypeta ashei", "Bolitogyrus buphthalmus"),
 "Eurasia" => array("Saiga tatarica", "Endromidae", "Pygeretmus zhitkovi"));
 
@@ -12,7 +12,7 @@ $double_name = [];
 $continent = [];
 $first_world = [];
 $second_word = [];
-echo '<h1>Массив животных через запятую</h1>';
+/*echo '<h1>Массив животных через запятую</h1>';
 foreach ($continents as $key => $animals) {
     $continent[] = $key;
     echo "<h2>$key</h2>";
@@ -25,6 +25,26 @@ foreach ($continents as $key => $animals) {
             $second_word[] = $rows_two[1];
 		}
     }
+}*/
+echo '<h1>Массив животных по континентам</h1>';
+foreach ($continents as $key => $animals) {
+    $continent[] = $key;
+    echo ("<h2>$key</h2>".
+		"<ul>");
+    foreach($animals as $animal)
+	{
+		echo "<li>$animal</li>";
+	}
+	echo "</ul>";
+    foreach ($animals as $two) {
+        $rows_two = explode(" ", $two);
+        if (count($rows_two) == 2) {
+            $double_name[] = $two;
+            $first_world[] = $rows_two[0];
+            $second_word[] = $rows_two[1];
+		}
+    }
+
 }
 echo '<hr>';
 echo '<h1>Массив из двух слов</h1>';
